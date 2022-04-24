b9e448cc1f1a72ea064903ac6918f60bf514483e
Huungociter <ckem1stc@gmail.com>
2022-04-24 22:04:26 +0700

    reset password by email and end course

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -132,3 +132,10 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ckem1stc@gmail.com'
+EMAIL_HOST_PASSWORD = '123'
