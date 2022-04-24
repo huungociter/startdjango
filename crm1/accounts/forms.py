afed99b5c51634521f446142e4bace9349567003
Huungociter <ckem1stc@gmail.com>
2022-04-24 20:36:05 +0700

    pemission group admin and customer, add picture to profile

--- crm1/accounts/forms.py
+++ crm1/accounts/forms.py
@@ -2,13 +2,20 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Order
 
+from .models import *
+
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
 	class Meta:
 		model = Order
 		fields = '__all__'
+
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
